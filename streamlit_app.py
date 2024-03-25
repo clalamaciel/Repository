6e4a9737d4771b664d0e0bd3d50906d3d3bad719
clalamaciel <162983236+clalamaciel@users.noreply.github.com>
2024-03-25 11:43:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-voc=pd.read_csv( https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
 l=voc.shape[0]
-indices=np.random.choice(l,size=4,replace=False)
-j=np.random.choice(indices)
+i=np.random.choice(l,size=4,replace=False)
 word_fr=voc['Définition'].values[j]
+word_chi=voc['Hanzi'].values[i]
+st.write(word_fr+"Hanzi"+word_chi)
+j=np.random.choice(indices)
+word_fr=voc["Définition"]
 st.write("Traduis:"+"+word_fr)
 def is_correct(i,j):
   if i==j:
